9df9ae07ae092f059e8e4a45edcaf0d0ce13d5fc
lesha K <alexei.jobs@gmail.com>
2018-09-17 07:33:54 +0300

    Add GreenAdapter. Not wrk yet

--- app/src/main/java/com/example/android/viewpager/GreenAdapter.java
+++ app/src/main/java/com/example/android/viewpager/GreenAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.viewpager;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
